d9828dd7624802c254276322cc7edea63ee6ab1a
Fazal-Rehaman07 <fazalurrehaman@gmail.com>
2024-09-27 16:28:12 -0400

    Add Function fibonacci.go

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Welcome to Group A's Week 4 Project!")
+    fmt.Println("Welcome to Group F's Week 4 Project!")
+
+	n := 20 
+    fmt.Printf("Fibonacci series till %d numbers:\n", n)
+    fibonacci(n)
 }
