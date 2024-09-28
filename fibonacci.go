3a3524a2bb169de8fc9d836d8634c14715e024a6
Fazal-Rehaman07 <fazalurrehaman@gmail.com>
2024-09-27 22:12:17 -0400

    Update main function to include menu and take input from user

--- fibonacci.go
+++ fibonacci.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func fibonacci(n int) {
-    a, b := 0, 1
-    for i := 0; i < n; i++ {
-        fmt.Print(a, " ")
+    a := 0
+    b := 1
+    nxt := a + b
+    fmt.Printf("%d %d ", a,b)
+
+    for i := 3; i <=n; i++ {
+        fmt.Print(nxt, " ")
         a = b
-		b = a+b
+		b = nxt
+        nxt = a + b
     }
+    
     fmt.Println()
 }
--- main.go
+++ main.go
@@ -3,9 +3,40 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Welcome to Group F's Week 4 Project!")
+    fmt.Println("Welcome to Group F's Week 4 Project!\n")
 
-	n := 20 
-    fmt.Printf("Fibonacci series till %d numbers:\n", n)
-    fibonacci(n)
+    var choice int
+    fmt.Println("Select the Function to Run")
+    fmt.Println("1. Fibonacci Series")
+    fmt.Println("2. Palindrome")
+    fmt.Println("3. Diamond Pattern")
+
+    fmt.Println("\nEnter Your Choice: ")
+	fmt.Scan(&choice)
+    
+    switch choice{
+    case 1:
+        var n int
+        fmt.Print("Enter the Limit (Positive No.) to Display Fibonacci Series: ")
+	    fmt.Scan(&n)
+        fmt.Printf("Fibonacci series till %d numbers:\n", n)
+        fibonacci(n)
+
+    case 2:
+        var input int
+        fmt.Print("Enter a number: ")
+	    fmt.Scan(&input)
+	    if palindrome(input) {
+		fmt.Println("The number is a palindrome!")
+	    } else {
+		fmt.Println("The number is not a palindrome.")
+	    }
+
+    default:
+        fmt.Println("Please try again by selecting a value between 1 and 5")
+
+    }
+    
+    
+    
 }
