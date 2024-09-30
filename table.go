4d08a9c5c744f0c5ce9971e3d119816243f05e30
Sahil Kumar Thakyal <Skthakyal27@gmail.com.com>
2024-09-29 21:28:19 -0400

    Sahil : add table function to repository

--- table.go
+++ table.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func Table(n int) {
-	fmt.Println("Multiplication table:")
+	fmt.Printf("\nMultiplication table of %d :\n", n)
 	for i := 1; i <= 10; i++ {
 		fmt.Printf("%d x %d = %d\n", n, i, n*i)
 	}
