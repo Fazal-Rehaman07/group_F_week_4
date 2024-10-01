3b6bb89bbb858070bef2bf7052ae5513e41749d2
Fazal-Rehaman07 <88478242+Fazal-Rehaman07@users.noreply.github.com>
2024-09-30 20:08:11 -0400

    Add comments to fibonacci.go

--- fibonacci.go
+++ fibonacci.go
@@ -6,14 +6,14 @@ func fibonacci(n int) {
 	a := 0
 	b := 1
 	nxt := a + b
-	fmt.Printf("%d %d ", a, b)
+	fmt.Printf("%d %d ", a, b) //print the first two numbers in the Fibonacci series
 
 	for i := 3; i <= n; i++ {
-		fmt.Print(nxt, " ")
-		a = b
+		fmt.Print(nxt, " ") //print the next number in the series
+		a = b 
 		b = nxt
-		nxt = a + b
+		nxt = a + b //calculate the next number in the series
 	}
 
-	fmt.Println()
+	fmt.Println() 
 }
